777d8f56e3f5fbea119f823cd01e60da37203a4c
Yuri Camara <yuricamara@gmail.com>
2018-03-31 17:10:42 -0300

    Suporte IE

---
+++ app/scripts/ie/ie.js
@@ -0,0 +1,21 @@
+var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+var isIEtill10 = navigator.userAgent && navigator.userAgent.indexOf('MSIE') != -1;
+
+if(isIEtill10 || isIE11){
+  console.log('IE Browser Detected');
+
+  // Carrega e executa Polyfill pro SVG Externo
+  // ******************************************
+  var el = document.createElement('script');
+  el.type = 'text/javascript';
+  el.src = 'scripts/ie/polyfills/svg4everybody.min.js';
+  el.onload = el.onreadystatechange = function() {
+    var rs = this.readyState;
+    if (rs && rs !== 'complete' && rs !== 'loaded') return;
+    svg4everybody();
+  };
+
+  var script = document.getElementsByTagName('script')[0];
+  script.parentNode.insertBefore(el, script);
+}
+
