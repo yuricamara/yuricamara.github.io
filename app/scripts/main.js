19a8b5ef5c168f8c5e3daecb336b84fc6a00982c
Yuri Camara <yuricamara@gmail.com>
2018-04-17 22:08:20 -0300

    window.addEventListener('scroll')

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,22 +6,32 @@
   const $menuToggleOpen = document.getElementById('m-nav_menu-toggle-open');
   const $menuToggleClose = document.getElementById('m-nav_menu-toggle-close');
 
+  let isNavItemsVisible = false;
+
   const showNavItems = () => {
     $navItems.style.display = 'block';
     $menuToggleOpen.style.display = 'none';
     $menuToggleClose.style.display = 'block';
+    isNavItemsVisible = true;
   };
 
   const hideNavItems = () => {
     $navItems.style.display = 'none';
     $menuToggleOpen.style.display = 'block';
     $menuToggleClose.style.display = 'none';
+    isNavItemsVisible = false;
   };
 
   $menuToggleOpen.addEventListener('click', showNavItems, false);
 
   $menuToggleClose.addEventListener('click', hideNavItems, false);
   $navItems.addEventListener('click', hideNavItems, false);
+
+  window.addEventListener('scroll', () => {
+    if(isNavItemsVisible){
+      hideNavItems();
+    }
+  }, false);
 
   // ******************************
 }
