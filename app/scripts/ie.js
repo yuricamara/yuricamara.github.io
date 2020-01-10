e2c18a6d4a7eeff422cf4aeafbf68bd866608fb9
Yuri Câmara <yuricamara@gmail.com>
2020-01-10 08:48:48 -0300

    ie

--- app/scripts/ie.js
+++ app/scripts/ie.js
@@ -1,23 +1,12 @@
 (function () {
   var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
-  var isIEtill10 = navigator.userAgent && navigator.userAgent.indexOf('MSIE') != -1;
+  var isIEtill10 = navigator.userAgent && navigator.userAgent.indexOf('MSIE') !== -1;
 
   if(isIEtill10 || isIE11){
-    console.log('IE Browser Detected');
-
-    // Carrega e executa Polyfill pro SVG Externo
+    // Exibe aviso que está usando IE
     // ******************************************
-    var el = document.createElement('script');
-    el.type = 'text/javascript';
-    el.src = '/dist/scripts/polyfills/svg4everybody.min.js';
-    el.onload = el.onreadystatechange = function() {
-      var rs = this.readyState;
-      if (rs && rs !== 'complete' && rs !== 'loaded') return;
-      svg4everybody();
-    };
-
-    var script = document.getElementsByTagName('script')[0];
-    script.parentNode.insertBefore(el, script);
+    document.getElementById('ie')
+      .setAttribute('style', 'display:block');
   }
 })();
 
