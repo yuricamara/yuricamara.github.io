62178c5df7ccbf18b539649a539d60a7411f100e
Yuri Câmara <yuricamara@gmail.com>
2020-01-14 11:45:23 -0300

    (href|src|srcset)="?\/[^\/\s"]+','gi')

--- gulpfile.js
+++ gulpfile.js
@@ -134,10 +134,11 @@ gulp.task('html:dist', ['rev'], () => {
     .pipe($.revReplace({manifest: manifest}))
     // Substitui href após minificação e revReplace
     .pipe($.stringReplace(
-      new RegExp('(href|src|srcset)="?\/[^\/\s"]+','gi'),
-      hrefMatched => {
-        return hrefMatched.replace(/\//, '/dist/');
-      }
+      '/images','/dist/images'
+      // new RegExp('(href|src|srcset)="?\/[^\/\s"]+','gi'),
+      // hrefMatched => {
+      //   return hrefMatched.replace(/\//, '/dist/');
+      // }
     ))
     .pipe(gulp.dest("./"));
 });
